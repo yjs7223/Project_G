a0338505206893820eaa5b676ca58e836fee35a6
sero <yjs7223@gmail.com>
2024-03-12 15:51:32 +0900

    v0.1
    init

---
+++ Source/Project_G/Project_G.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_G.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_G, "Project_G" );
